f9c12b8ebba60e96329844926c1d6a5e16dff86c
ern-ocampo <ernest.s.ocampo@gmail.com>
2023-03-06 09:14:57 +0800

    push

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from 'app/album';
+import { ProductService } from 'app/product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -7,12 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-//   albumInfo: Album;
+  albumInfo: Album;
 
-//   constructor( private _productService: ProductService) { }
+  constructor( private _productService: ProductService) { }
 
   ngOnInit() {
-    // this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
