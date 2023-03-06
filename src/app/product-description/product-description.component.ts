e1d29e9c0b5d9e3e6c4b06a87d18d1911b7ad6a6
ern-ocampo <ernest.s.ocampo@gmail.com>
2023-03-06 08:59:24 +0800

    push

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+
+//   albumInfo: Album;
+
+//   constructor( private _productService: ProductService) { }
+
+  ngOnInit() {
+    // this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+  }
+
+}
